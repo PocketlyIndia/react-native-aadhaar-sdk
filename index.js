7761b3b5f2cff9a3d30d78e4bf9b1c0b882ed3ee
Navdeesh Ahuja <navdeeshahuja@gmail.com>
2020-09-11 02:39:12 +0530

    Add basic stuff to make it atleast a package

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class AadharVerificationView extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Hello</Text>
+            </View>
+        );
+    }
+}
+
+
